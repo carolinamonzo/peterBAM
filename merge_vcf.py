ad3ad65644469b9292f8a7ad692438becdc8e087
carolina <carolmonzoc@gmail.com>
2018-07-18 13:27:27 +0200

    made parallel silent

--- merge_vcf.py
+++ merge_vcf.py
@@ -102,8 +102,8 @@ def run_parallel(config, cmd_sh):
 
     # Execute command
 
-    #subprocess.call(cmd + " 2> /dev/null", shell = True)
-    subprocess.call(cmd, shell=True)
+    subprocess.call(cmd + " 2> /dev/null", shell = True)
+    
 
 
 def write_output_fof(config):
